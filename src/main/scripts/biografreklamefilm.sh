532b17cf18872f843adef35b4c59d351e0daa330
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2012-12-10 09:56:32 +0100

    Add packaging

---
+++ src/main/scripts/biografreklamefilm.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+BASEDIR=$(readlink -f $(dirname $0))/..
+
+java -classpath $BASEDIR/lib/*:$BASEDIR/config dk.statsbiblioteket.doms.ingest.reklamepbcoremapper.BiografPBCoreMapperUtil "$@"
